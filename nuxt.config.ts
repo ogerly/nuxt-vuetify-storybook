53583349101bffd3c2d6ad08fc94080f2f1c0f71
ogerly <ogerly@tutanota.com>
2023-10-28 08:21:14 +0200

    change vite-plugin-vuetify to vuetify-nuxt-module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,7 @@
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  build: {
-    transpile: ['vuetify'],
-  },
   modules: [
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
-    },
-    //...
-  ],
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
-    },
-  },
+    'vuetify-nuxt-module'
+  ]
 })
