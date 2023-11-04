79c4e3f81f192f6efcd92d036597bf94b4ff0960
ogerly <ogerly@tutanota.com>
2023-11-04 11:19:00 +0100

    add eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  plugins: [],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "vue/require-default-prop": "off",
+  },
+};
